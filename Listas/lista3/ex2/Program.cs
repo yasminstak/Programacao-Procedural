1b9f4c8ace9814383724ca73822d172841d3efe6
yasmin.stak <yasmin.camargo25@gmail.com>
2024-03-08 22:28:09 -0300

    enviando lista 3

---
+++ Listas/lista3/ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  {
+    int contador = 1;
+    double somatorioValores = 0;
+    double totalItensComprados = 0;
+
+    Console.WriteLine("Digite a quantidade de itens que voce comprou: ");
+    int itensComprados = int.Parse(Console.ReadLine());
+
+    while (contador <= itensComprados)
+    {
+      Console.WriteLine("Digite o valor do " + contador + " º item: R$");
+      double valorItem = double.Parse(Console.ReadLine());
+      totalItensComprados = totalItensComprados + valorItem;
+      contador ++;
+    
+    }
+    if (totalItensComprados >= 150)
+    {
+        System.Console.WriteLine("parabens! voce ganhou frete gratis em sua compra de valor: " + 
+        Math.Round(totalItensComprados));
+    }
+    else
+    {
+        System.Console.WriteLine("Que pena! sua compra nao possui frete gratis");
+    }
+      
+    }
+  }
---
+++ Listas/lista3/ex3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        const int NUMERO_DE_HABITANTES = 20;
+        double mediaSalarial  = 20;
+        double maiorSalario = 0;
+        double somatorioSalarial = 0;
+        int somatoriaFilhos = 0;
+        double mediaFilhos = 0;
+
+        for (int contador = 1; contador <= NUMERO_DE_HABITANTES; contador++)
+         {
+            System.Console.WriteLine("Digite o seu salario: ");
+            double salario = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite a quantidade de filhos: ");
+            int numeroDeFilhos = int.Parse(Console.ReadLine());
+
+            somatorioSalarial = somatorioSalarial + salario;
+            somatoriaFilhos = somatoriaFilhos + numeroDeFilhos;
+            if (maiorSalario > salario)
+            {
+                salario = maiorSalario;
+            } 
+        
+         }
+            mediaSalarial = somatorioSalarial / NUMERO_DE_HABITANTES;
+            mediaFilhos = somatoriaFilhos / NUMERO_DE_HABITANTES;
+            System.Console.WriteLine("A media salarial é: " + mediaSalarial);
+            System.Console.WriteLine("O maior salario é: " + maiorSalario);
+            System.Console.WriteLine("A media de filhos é " + mediaFilhos);
+
+
+    }
+}
+
+
